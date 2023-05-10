6aa59b5f35d1bb1649d4b78b3d186d4506cc4705
MichaelJuannn <juan600d@gmail.com>
2023-05-10 18:50:46 +0700

    add react query and scientific page

---
+++ app/scientific/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useQuery } from 'react-query';
+
+export default function Scientific() {
+	async function getMed() {
+		const res = await fetch('https://methlab.vercel.app/api/obat');
+		const med = await res.json();
+		return med;
+	}
+
+	const { data, isLoading, error } = useQuery('medicines', getMed);
+	return <>{'ded'}</>;
+}
