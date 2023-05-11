09b540f5815137637961be16a9b5ee9577188bc3
Eka Robyanto <robyantoeka@gmail.com>
2023-05-11 10:19:10 +0700

    homepage

--- app/scientific/page.tsx
+++ app/scientific/page.tsx
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 
 export default function Scientific() {
-	async function getMed() {
-		const res = await fetch('https://methlab.vercel.app/api/obat');
-		const med = await res.json();
-		return med;
-	}
+  async function getMed() {
+    const res = await fetch("https://methlab.vercel.app/api/obat");
+    const med = await res.json();
+    console.log(med);
+    return med;
+  }
 
-	const { data, isLoading, error } = useQuery('medicines', getMed);
-	return <>{'ded'}</>;
+  const { data, isLoading, error } = useQuery("medicines", getMed);
+  return <>{"ded"}</>;
 }
